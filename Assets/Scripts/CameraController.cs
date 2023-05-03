59d0956681ea6d2318a7f09e6caf38168ef13dc9
bdf007 <bdf007@gmail.com>
2023-05-03 09:33:11 +0200

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+
+    public float minXRot, maxXRot;
+
+    private float curXrot;
+
+    public float minZoom, maxZoom;
+
+    public float zoomSpeed;
+    public float rotateSpeed;
+
+    private float curZoom;
+    private Camera cam;
+
+    private void Start()
     {
-        
+        cam = Camera.main;
+        curZoom = cam.transform.localPosition.y;
+        curXrot = -50;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // zooming
+        curZoom += Input.GetAxis("Mouse ScrollWheel") * -zoomSpeed;
+        curZoom = Mathf.Clamp(curZoom, minZoom, maxZoom);
+
+        cam.transform.localPosition = Vector3.up * curZoom;
+
+        // camera look
+        if (Input.GetMouseButton(1))
+        {
+            float x = Input.GetAxis("Mouse X") * rotateSpeed;
+            float y = Input.GetAxis("Mouse Y") * rotateSpeed;
+           
+        }
     }
 }
